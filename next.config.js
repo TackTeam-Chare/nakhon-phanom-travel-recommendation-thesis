a4cb3851310889993b557ba1a41102890099b83b
TackTeam-Chare <chare.auppachitwan@gmail.com>
2024-09-26 16:12:15 +0700

    renew

--- next.config.js
+++ next.config.js
@@ -11,14 +11,7 @@ module.exports = {
       'tailwindui.com', 
       'www.thaitravelcenter.com'
     ],
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '5000',
-        pathname: '/public/uploads/**',
-      },
-    ],
     unoptimized: true,
   },
 };
+
